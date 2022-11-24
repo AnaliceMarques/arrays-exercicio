388ec04482dd553300b59a63422b24dc4b7068a0
AnaliceMarques <analicemarquessl@gmail.com>
2022-11-24 11:46:23 -0300

    Resolução do Exercício 1

---
+++ exercicio1/script.js
@@ -0,0 +1,21 @@
+// No primeiro exercício vamos praticar a criação de arrays. Crie 3 arrays diferentes:
+
+// - O primeiro deve possuir apenas **números**, e estes números não devem seguir nenhuma ordem específica;
+
+const arrayNumeros = [15, 72, 60, 33, 24, 12];
+
+// - O segundo array deve possuir apenas **strings**;
+
+const arrayStrings = ["copa", "brasil", "hexa", "campeão"];
+
+// - O terceiro array deve possuir **números**, **strings** e **booleanos**.
+
+const arrayTiposDif = ["seleção", 6, true];
+
+// Imprima cada um dos arrays acima, utilizando `console.log()`
+
+console.log(arrayNumeros);
+
+console.log(arrayStrings);
+
+console.log(arrayTiposDif);
